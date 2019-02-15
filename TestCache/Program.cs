565b33181fe8d6c87a23f45d4d3f7fcb27434f3e
lixindou <Lixin.dou@hotmail.com>
2019-02-14 19:06:30 -0500

    TestCache

---
+++ TestCache/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using LazyCache;
+using TestCache.CsbMigration.OAuthService;
+
+namespace TestCache
+{
+    class Program
+    {
+        private DateTimeOffset testTime;
+        static void Main(string[] args)
+        {
+            var testObject = new Test();
+
+            var i = testObject.GetCachedObject();
+            Console.WriteLine(i);
+            Thread.Sleep(3000);
+            i = testObject.GetCachedObject();
+            Console.WriteLine(i);
+        }
+    }
+
+
+}
---
+++ TestCache/Test.cs
@@ -0,0 +1,37 @@
+﻿namespace TestCache
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Runtime;
+    using LazyCache;
+
+    namespace CsbMigration.OAuthService
+    {
+        public class Test
+        {
+            private int cacheTimeInSeconds = 5;
+            private IAppCache cache;
+            public Test()
+            {
+                this.cache = new CachingService();
+            }
+           public string GetCachedObject()
+            {
+                Func<Task<string>> fetchObjectFunc = FetchObject;
+                var token = this.cache
+                    .GetOrAdd("Token", fetchObjectFunc, DateTimeOffset.UtcNow.AddSeconds(this.cacheTimeInSeconds))
+                    .GetAwaiter()
+                    .GetResult();
+                return token;
+            }
+
+            private Task<string> FetchObject()
+            {
+                cacheTimeInSeconds = 5;
+                Console.WriteLine("Get object");
+                return Task.FromResult(DateTimeOffset.UtcNow.ToString("o"));
+            }
+        }
+    }
+}
